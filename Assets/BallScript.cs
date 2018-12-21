3cb903679c608a641e6f0c42ca9d36f838cbac1b
Eric Becking <eric_becking@ign.com>
2018-12-21 12:29:59 -0800

    initial commit

---
+++ Assets/BallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+  public float speed = 10f;
+  public float maxSpeed = 20f;
+  Rigidbody2D rb;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+
+    Vector2 direction = new Vector2(Random.Range(-1.0f, 0.0f), Random.Range(-1.0f, 0.0f));
+    Move(direction);
+  }
+
+  void FixedUpdate()
+  {
+    //ensure that the speed of the object never gets totally out of hand
+    float speed = rb.velocity.magnitude;
+    rb.velocity = rb.velocity / speed * Mathf.Min(speed, maxSpeed);
+
+    //if it ever stops moving left to right, give it some left acceleration
+    if (rb.velocity.x == 0.0)
+    {
+      rb.AddForce(transform.right * Random.Range(-2.0f, 2.0f));
+    }
+
+    if (rb.velocity.y == 0.0)
+    {
+      rb.AddForce(transform.up * Random.Range(-2.0f, 2.0f));
+    }
+
+  }
+
+  void Move(Vector2 direction)
+  {
+    rb.velocity = direction * speed;
+  }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+  public float paddleSpeed = 10f;
+  public KeyCode upKey;
+  public KeyCode downKey;
+
+  void Update()
+  {
+    //if pressing up go up
+    if (Input.GetKey(upKey))
+    {
+      MoveUp();
+    }
+    else if (Input.GetKey(downKey))
+    {
+      MoveDown();
+    }
+    else
+    {
+      Move(Vector2.zero);
+    }
+  }
+
+  void MoveUp()
+  {
+    Move(Vector2.up);
+  }
+
+  void MoveDown()
+  {
+    Move(Vector2.down);
+  }
+
+  void Move(Vector2 direction)
+  {
+    transform.Translate(direction * paddleSpeed * Time.deltaTime);
+  }
+
+}
